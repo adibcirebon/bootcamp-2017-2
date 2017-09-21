9ee13ef194e6623e61caea69b35bbb36bf2fb7a4
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-21 15:29:53 +0700

    membuat template project jdbc

---
+++ bootcamp-jdbc/src/main/java/com/dimas/maryanto/App.java
@@ -0,0 +1,24 @@
+package com.dimas.maryanto;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.SQLException;
+
+public class App {
+
+    private final static String url = "jdbc:postgresql://localhost:5432/bootcamp_2";
+    private final static String username = "bootcamp_2";
+    private final static String password = "bootcamp";
+    private final static String driverClassName = "org.postgresql.Driver";
+
+    public static void main(String[] args) throws SQLException {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(url);
+        ds.setPassword(password);
+        ds.setUsername(username);
+        ds.setDriverClassName(driverClassName);
+
+        ds.getConnection();
+        System.out.println("Hello it works!");
+    }
+}
