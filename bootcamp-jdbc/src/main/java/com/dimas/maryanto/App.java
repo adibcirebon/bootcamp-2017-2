ebef19c9fb8e29e5c72af869dd2f744d8c344fe1
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-22 14:19:29 +0700

    membuat template project dan jdbc delete

--- bootcamp-jdbc/src/main/java/com/dimas/maryanto/App.java
+++ bootcamp-jdbc/src/main/java/com/dimas/maryanto/App.java
@@ -1,8 +1,12 @@
 package com.dimas.maryanto;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class App {
+
 
     private final static String url = "jdbc:postgresql://localhost:5432/bootcamp_2";
     private final static String username = "bootcamp_2";
@@ -10,6 +14,7 @@ public class App {
     private final static String driverClassName = "org.postgresql.Driver";
 
     public static void main(String[] args) throws SQLException {
+
 
 //        BasicDataSource ds = new BasicDataSource();
 //        ds.setUrl(url);
@@ -21,23 +26,33 @@ public class App {
 //        Connection koneksiDb = ds.getConnection();
         Connection koneksiDb = DriverManager.getConnection(url, username, password);
 
-        String sql = "select id, uname, fullname, email, tanggal_lahir from s_user";
-        Statement statement = koneksiDb.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
+        String sql = "delete from s_user where id = ?";
+//        System.out.println(sql);
 
-//        load data dari server
-        while (resultSet.next()) {
-            Integer idUser = resultSet.getInt("id");
-            String username = resultSet.getString("uname");
-            Date tanggalLahir = resultSet.getDate("tanggal_lahir");
+        PreparedStatement ps = koneksiDb.prepareStatement(sql);
+        ps.setInt(1, 3);
+        ps.executeUpdate();
 
-            System.out.println("userid " + idUser + ", username " + username + ", tanggal lahir " + tanggalLahir);
-        }
-
-//        tutup semua koneksi
-        resultSet.close();
-        statement.close();
+        ps.close();
         koneksiDb.close();
+
+
+//        String sql = "select id, uname, fullname, email, tanggal_lahir from s_user";
+//        Statement statement = koneksiDb.createStatement();
+//        ResultSet resultSet = statement.executeQuery(sql);
+//
+//        load data dari server
+//        while (resultSet.next()) {
+//            Integer idUser = resultSet.getInt("id");
+//            String username = resultSet.getString("uname");
+//            Date tanggalLahir = resultSet.getDate("tanggal_lahir");
+//
+//            System.out.println("userid " + idUser + ", username " + username + ", tanggal lahir " + tanggalLahir);
+//        }
+//
+//        tutup semua koneksi
+//        resultSet.close();
+//        statement.close();
 
         System.out.println("Hello it works!");
     }
---
+++ bootcamp-jdbc/src/main/java/com/dimas/maryanto/HaloJava.java
@@ -0,0 +1,14 @@
+package com.dimas.maryanto;
+
+public class HaloJava {
+
+    public static void sayHalo() {
+        System.out.println("Halo ini dari kelas HaloJava");
+    }
+
+    public String asdlfkjsdaf() {
+        System.out.println("Halo ini dari kelas HaloJava");
+        return "Halo ini nilai kembalian";
+//        System.out.println();
+    }
+}
