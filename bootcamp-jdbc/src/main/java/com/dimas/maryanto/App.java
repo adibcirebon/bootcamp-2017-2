0a4962f22734a2a3c5de2d15688e1123dbd59a20
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-22 11:24:18 +0700

    ignore file hasil compile dengan .gitignore

--- bootcamp-jdbc/src/main/java/com/dimas/maryanto/App.java
+++ bootcamp-jdbc/src/main/java/com/dimas/maryanto/App.java
@@ -1,8 +1,6 @@
 package com.dimas.maryanto;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App {
 
@@ -12,13 +10,35 @@ public class App {
     private final static String driverClassName = "org.postgresql.Driver";
 
     public static void main(String[] args) throws SQLException {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(url);
-        ds.setPassword(password);
-        ds.setUsername(username);
-        ds.setDriverClassName(driverClassName);
 
-        ds.getConnection();
+//        BasicDataSource ds = new BasicDataSource();
+//        ds.setUrl(url);
+//        ds.setPassword(password);
+//        ds.setUsername(username);
+//        ds.setDriverClassName(driverClassName);
+//
+//        buka koneksi ke database
+//        Connection koneksiDb = ds.getConnection();
+        Connection koneksiDb = DriverManager.getConnection(url, username, password);
+
+        String sql = "select id, uname, fullname, email, tanggal_lahir from s_user";
+        Statement statement = koneksiDb.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+//        load data dari server
+        while (resultSet.next()) {
+            Integer idUser = resultSet.getInt("id");
+            String username = resultSet.getString("uname");
+            Date tanggalLahir = resultSet.getDate("tanggal_lahir");
+
+            System.out.println("userid " + idUser + ", username " + username + ", tanggal lahir " + tanggalLahir);
+        }
+
+//        tutup semua koneksi
+        resultSet.close();
+        statement.close();
+        koneksiDb.close();
+
         System.out.println("Hello it works!");
     }
 }
