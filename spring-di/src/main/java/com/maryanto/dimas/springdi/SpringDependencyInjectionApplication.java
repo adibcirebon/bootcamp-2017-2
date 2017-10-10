7f557feedf2217926e1e89d9adf0f985e25d9e95
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-10 13:33:31 +0700

    membuat bean di spring container

--- spring-di/src/main/java/com/maryanto/dimas/springdi/SpringDependencyInjectionApplication.java
+++ spring-di/src/main/java/com/maryanto/dimas/springdi/SpringDependencyInjectionApplication.java
@@ -1,6 +1,5 @@
 package com.maryanto.dimas.springdi;
 
-import com.maryanto.dimas.springdi.no.spring.KelasDao;
 import com.maryanto.dimas.springdi.spring.KonfigurasiSpring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,7 +8,18 @@ public class SpringDependencyInjectionApplication {
 
     public static void main(String[] x) {
         ApplicationContext springContainer = new AnnotationConfigApplicationContext(KonfigurasiSpring.class);
-        KelasDao kelasDao = springContainer.getBean(KelasDao.class);
-        kelasDao.save();
+
+        String seqUUID = springContainer.getBean(String.class);
+//        String seqUUID = UUID.randomUUID().toString();
+        System.out.println(seqUUID);
+
+        seqUUID = springContainer.getBean(String.class);
+//        seqUUID = UUID.randomUUID().toString();
+        System.out.println(seqUUID);
+
+        seqUUID = springContainer.getBean(String.class);
+//        seqUUID = UUID.randomUUID().toString();
+        System.out.println(seqUUID);
+
     }
 }
--- spring-di/src/main/java/com/maryanto/dimas/springdi/spring/KonfigurasiSpring.java
+++ spring-di/src/main/java/com/maryanto/dimas/springdi/spring/KonfigurasiSpring.java
@@ -5,6 +5,9 @@ import com.maryanto.dimas.springdi.no.spring.Koneksi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.UUID;
 
 @Configuration
 public class KonfigurasiSpring {
@@ -17,11 +20,18 @@ public class KonfigurasiSpring {
     public String password;
 
     @Bean
+    @Scope("prototype")
+    public String message(){
+        return UUID.randomUUID().toString();
+    }
+
+    @Bean
     public Koneksi getKoneksi() {
         return new Koneksi(url, username, password);
     }
 
     @Bean
+    @Scope("prototype")
     public KelasDao getKelasDao(Koneksi koneksi) {
         return new KelasDao(koneksi);
     }
