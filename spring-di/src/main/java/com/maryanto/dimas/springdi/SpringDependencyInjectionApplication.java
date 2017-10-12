d115d33ac5e3ff50e320ac9e8c9cc530a566f86e
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-12 10:10:17 +0700

    migrasi ke spring boot

--- spring-di/src/main/java/com/maryanto/dimas/springdi/SpringDependencyInjectionApplication.java
+++ spring-di/src/main/java/com/maryanto/dimas/springdi/SpringDependencyInjectionApplication.java
@@ -1,16 +1,12 @@
 package com.maryanto.dimas.springdi;
 
-import com.maryanto.dimas.springdi.no.spring.InnerKoneksi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDependencyInjectionApplication {
 
     public static void main(String[] x) {
-        ApplicationContext springContext = SpringApplication.run(SpringDependencyInjectionApplication.class, x);
-        InnerKoneksi innerKoneksi = springContext.getBean("innerKoneksi", InnerKoneksi.class);
-        System.out.println(innerKoneksi.getSayHalo());
+        SpringApplication.run(SpringDependencyInjectionApplication.class, x);
     }
 }
---
+++ spring-di/src/main/java/com/maryanto/dimas/springdi/repository/ManufactureRepository.java
@@ -0,0 +1,7 @@
+package com.maryanto.dimas.springdi.repository;
+
+import com.maryanto.dimas.springdi.model.Manufacture;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ManufactureRepository extends CrudRepository<Manufacture, String> {
+}
