cf79bc2643ca8a7d4ae369dc1d2b21b281a6d1bb
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-12 10:28:30 +0700

    soal nomor 3

---
+++ spring-di/src/main/java/com/maryanto/dimas/springdi/Application.java
@@ -0,0 +1,41 @@
+package com.maryanto.dimas.springdi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] x) {
+        ApplicationContext springContect = SpringApplication.run(Application.class, x);
+        UmurSaya bean = springContect.getBean(UmurSaya.class);
+        System.out.println(bean.umurSaya());
+    }
+
+    @Bean
+    public Integer umur() {
+        return 20;
+    }
+
+    @Bean(name = "umurKe25")
+    public Integer umur25() {
+        return 25;
+    }
+
+    @Component
+    public class UmurSaya {
+
+        @Autowired
+        @Qualifier("umurKe25")
+        private Integer umur;
+
+        public String umurSaya() {
+            return "Umur saya " + umur + " tahun";
+        }
+    }
+}
--- spring-di/src/main/java/com/maryanto/dimas/springdi/repository/ManufactureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maryanto.dimas.springdi.repository;
-
-import com.maryanto.dimas.springdi.model.Manufacture;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ManufactureRepository extends CrudRepository<Manufacture, String> {
-}
