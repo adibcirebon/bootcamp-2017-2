8f30e5e95eea6d3adb7451b3cf1c7f3850585fd1
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-12 16:03:52 +0700

    membuat spring web mvc dengan spring boot

--- spring-di/src/main/java/com/maryanto/dimas/springdi/Application.java
+++ spring-di/src/main/java/com/maryanto/dimas/springdi/Application.java
@@ -1,38 +1,33 @@
 package com.maryanto.dimas.springdi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-
 //@SpringBootApplication
 public class Application {
 
-    public static void main(String[] x) {
-        ApplicationContext springContect = SpringApplication.run(Application.class, x);
-        UmurSaya bean = springContect.getBean(UmurSaya.class);
-        System.out.println(bean.umurSaya());
-    }
-
-    //    @Bean
-    public Integer umur() {
-        return 20;
-    }
-
-    //    @Bean(name = "umurKe25")
-    public Integer umur25() {
-        return 25;
-    }
-
-    //    @Component
-    public class UmurSaya {
-
-        @Autowired
-        @Qualifier("umurKe25")
-        private Integer umur;
-
-        public String umurSaya() {
-            return "Umur saya " + umur + " tahun";
-        }
-    }
+//    public static void main(String[] x) {
+//        ApplicationContext springContect = SpringApplication.run(Application.class, x);
+//        UmurSaya bean = springContect.getBean(UmurSaya.class);
+//        System.out.println(bean.umurSaya());
+//    }
+//
+//    //    @Bean
+//    public Integer umur() {
+//        return 20;
+//    }
+//
+//    //    @Bean(name = "umurKe25")
+//    public Integer umur25() {
+//        return 25;
+//    }
+//
+//    //    @Component
+//    public class UmurSaya {
+//
+//        @Autowired
+//        @Qualifier("umurKe25")
+//        private Integer umur;
+//
+//        public String umurSaya() {
+//            return "Umur saya " + umur + " tahun";
+//        }
+//    }
 }
