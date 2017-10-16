0f82a0b157570e11284a786393320fc88b221f09
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-16 15:20:45 +0700

    mengakses user yang login dari controller

--- universitas-app/src/main/java/com/maryanto/dimas/controller/MahasiswaController.java
+++ universitas-app/src/main/java/com/maryanto/dimas/controller/MahasiswaController.java
@@ -1,5 +1,9 @@
 package com.maryanto.dimas.controller;
 
+import com.maryanto.dimas.model.User;
+import com.maryanto.dimas.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mahasiswa")
 public class MahasiswaController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/new")
-    public String newMahasiswa() {
+    public String newMahasiswa(Authentication auth) {
+//        username
+        User userLogin = userRepository.findByUsername(auth.getName());
+//        didalem username itu ada role apa saja?
+        auth.getAuthorities();
+
         return "/mahasiswa/TambahMahasiswa";
     }
 
